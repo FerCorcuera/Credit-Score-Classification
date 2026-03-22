aad4a3409607424f977eddd9cae3daa14d7347f6
Fer <188613791+FerCorcuera@users.noreply.github.com.>
2026-03-22 01:13:46 -0500

    cleaning: first function to clean numerical values done

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,16 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def remove_string(feature, char="_", new_char=""):
-    if pd.isna(feature):
-        return feature
+def cleaning_int_variables(feature, char="_", new_char="", new_type=float):
+    new_feature = feature.str.replace(char, new_char)
 
-    return feature.replace(char, new_char)
-
-
-def cleaning_int_variables(feature, char="_", new_char="", new_type=int):
-    new_feature = remove_string(feature, char, new_char)
-
-    new_feature = new_feature.astype(new_type)
-
-    return new_feature
+    return new_feature.astype(new_type)
--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -1,5 +1,17 @@
-from src.data_cleaning import remove_string
+import pandas as pd
+from src.data_cleaning import cleaning_int_variables
+import numpy as np
 
-word = "Student Loan __, Credit Loan"
 
-print(remove_string(word))
+def test_clean_numeric():
+    data = pd.Series(["123_", "45", "_2"])
+    data_2 = pd.Series(["123", "_", "5"])
+
+    result = cleaning_int_variables(data, "_", "", float)
+    result_2 = cleaning_int_variables(data_2, "_", "0", int)
+    expected = pd.Series([123.0, 45.0, 2.0])
+
+    expected_2 = pd.Series([123, 0, 5])
+
+    assert result.equals(expected)
+    assert result_2.equals(expected_2)
